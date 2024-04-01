92ac119e2f34f7b699dfafe32adbcb8511b4ff58
dan_tonye <danthurnye@gmail.com>
2024-04-01 18:51:21 -0400

    added contact

--- src/pages/CacaoCircles/CacaoCircles.js
+++ src/pages/CacaoCircles/CacaoCircles.js
@@ -4,7 +4,6 @@ import Box from '@mui/material/Box';
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import HeaderTitle from '../../components/HeaderTitle/HeaderTitle';
 import FoxyButton from '../../components/FoxyButton/FoxyButton';
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,27 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styles from './Home.module.css';
-import HeaderTitle from '../../components/HeaderTitle/HeaderTitle';
 import Box from '@mui/material/Box';
 import CardMedia from '@mui/material/CardMedia';
-import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import FoxyButton from '../../components/FoxyButton/FoxyButton';
-import { useTheme } from '@mui/material/styles';
-import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import IconButton from '@mui/material/IconButton';
-import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import SkipNextIcon from '@mui/icons-material/SkipNext';
-import Container from '@mui/material/Container';
 import Brands from '../../components/Brands/Brands'
+import portrait from '../../assets/photosAlexis2/foxy.JPG'
 
 const Home = () => {
-  const theme = useTheme();
-
   return(
   <Box className={styles.Home} sx={{
     height: 'calc(100vh - 100px)',
@@ -43,8 +31,8 @@ const Home = () => {
                   height: {xs: 200, sm: 300},
                   
                   borderRadius: '50%' }}
-                image="https://i.pinimg.com/originals/9c/0e/50/9c0e5033d2e9bac33502f0a03732ae9d.jpg"
-                alt="Live from space album cover"
+                image={portrait}
+                alt="Foxy"
               />
           </Box>
           <Box sx={{ display: 'flex', flexDirection: 'column', maxWidth: 'md' }}>
